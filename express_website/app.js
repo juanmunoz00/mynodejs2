d3585f0b1f5a34e1bc0a92a8e4b3cf3c84e61703
Juan Munoz <jmunoz@syner.info>
2021-05-21 15:18:19 -0600

    Added contact form, email working but need to solve the permissions.

--- express_website/app.js
+++ express_website/app.js
@@ -25,6 +25,39 @@ app.get('/about', function(req, res){
     res.render('about');
 });
 
+app.get('/contact', function(req, res){
+    res.render('contact');
+});
+
+app.post('/contact/send', function(req, res){
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'jmunoz@syner.info',
+            pass: 'juan11info'
+        }
+    });
+
+    var mailOptions = {
+        from: 'Juancho <jmunoz@syner.info>',
+        to: 'juan.arturo.munoz@gmail.com',
+        subject: 'Website sumission',
+        text: 'You have a submission with the following details... Name: ' + req.body.name + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
+        html: '<p>You have a submission with the following details...</p><ul><li>Name: '+ req.body.name +'</li><li>Email: '+ req.body.email +'</li><li>Message: '+ req.body.message +'</li></ul>'
+    }
+    
+    transporter.sendMail(mailOptions, function(error){
+        if(error){
+            console.log(error);
+            res.redirect('/');
+        }else{
+            console.log('Message sent: ' + info.response);
+            res.redirect('/');
+        }
+    });
+
+});
+
 //Port listen
 app.listen(port);
 console.log('Server is running on ${port}');
